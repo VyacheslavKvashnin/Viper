36985a78bc906c8417bf4374d36878264884d442
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-02 22:21:47 +0500

    Start project

--- Viper/GreetingViewController.swift
+++ Viper/GreetingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GreetingViewController: UIViewController {
 
     @IBOutlet weak var greetingLabel: UILabel!
     
---
+++ Viper/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Viper
+//
+//  Created by Вячеслав Квашнин on 02.05.2022.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
